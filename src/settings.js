efa5dbcc0d3dd918cd24cf11d194a8c6b7c1265d
Your Name <you@example.com>
2022-02-13 12:32:38 +0500

    )

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,4 @@
-const API_KEY = "d2d4270599d432f4007d4385c328a903";
-const API_URL = "https://api.openweathermap.org/data/2.5";
+const API_KEY = process.env.REACT_APP_API_KEY;
+const API_URL = process.env.REACT_APP_API_URL;
 
 export { API_KEY, API_URL };
