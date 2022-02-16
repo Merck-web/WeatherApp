a917da107d0b805554172776c70cd788005a7770
Your Name <you@example.com>
2022-02-16 18:57:44 +0500

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,20 +3,26 @@ import React from "react";
 import { useCitiesList } from "./hooks/useCitiesList";
 import { Input } from "./components/Input/Input";
 import { CardList } from "./components/CardList/CardList";
+import { BrowserRouter } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
 
 export const GlobalContext = React.createContext();
 
 function App() {
   const [state, dispatch] = useCitiesList();
   return (
-    <GlobalContext.Provider value={{ state, dispatch }}>
-      <div className='container'>
-        <div className='Main'>
-          <Input />
-          <CardList />
+    <BrowserRouter>
+      <GlobalContext.Provider value={{ state, dispatch }}>
+        <div className='container'>
+          <div className='Main'>
+            <Input />
+            <ErrorBoundary>
+              <CardList />
+            </ErrorBoundary>
+          </div>
         </div>
-      </div>
-    </GlobalContext.Provider>
+      </GlobalContext.Provider>
+    </BrowserRouter>
   );
 }
 
