34e82361f783feedddb960e671d3b85939c73fb7
Your Name <you@example.com>
2022-02-18 12:27:10 +0500

    useParams

--- src/components/SingleCity/SingleCity.jsx
+++ src/components/SingleCity/SingleCity.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link, useParams } from "react-router-dom";
 
 import "../../App.css";
+import { Card } from "../Card/Card";
 
 export const SingleCity = () => {
-  return <div>SingleCity</div>;
+  const { city } = useParams();
+  console.log(city);
+  return (
+    <>
+      <button>
+        <Link to={"/WeatherApp"}>Назад</Link>
+      </button>
+      <Card city={city} />
+    </>
+  );
 };
